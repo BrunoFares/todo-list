fe33292413646f3b360499165bf619eb61796b98
BrunoFares <faresbruno04@gmail.com>
2024-03-03 22:17:53 +0200

    Planned out the first skeleton of the website

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-import printMe from './layout.js';
+import {renderLayout} from './layout.js';
+import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-    const h1 = document.createElement('h1');
-
-    h1.innerHTML = 'title thing';
-
-    btn.innerHTML = 'Click me';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-    element.appendChild(h1);
-
-    return element;
-}
-
-document.body.appendChild(component());
+renderLayout();
--- src/layout.js
+++ src/layout.js
@@ -1,3 +1,33 @@
-export default function printMe() {
-    console.log('I get called from print.js');
+const div1 = document.createElement('div');
+div1.classList.add('banner');
+
+const h1 = document.createElement('h1');
+h1.innerHTML = 'To-Do List';
+const listsTitleHead = document.createElement('h2');
+listsTitleHead.innerHTML = 'My Lists';
+listsTitleHead.id = 'titleHead';
+div1.appendChild(h1);
+div1.appendChild(listsTitleHead);
+
+const div2 = document.createElement('div');
+div2.classList.add('sidebar');
+
+const h3 = document.createElement('h3');
+h3.innerHTML = 'My Lists';
+h3.id = 'titleSide';
+const newListDiv = document.createElement('div');
+newListDiv.innerHTML = 'Add new list';
+newListDiv.id = 'addItem';
+div2.appendChild(h3);
+div2.appendChild(newListDiv);
+
+const div3 = document.createElement('div');
+div3.classList.add('pitch');
+
+function renderLayout() {
+    document.body.appendChild(div1);
+    document.body.appendChild(div2);
+    document.body.appendChild(div3);
 }
+
+export {renderLayout};
