117c5670ed2cd0361afca6698e73da5004e52fc3
BrunoFares <faresbruno04@gmail.com>
2024-03-04 02:19:02 +0200

    Fixed the design of the layout page, with a dark theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import {renderLayout} from './layout.js';
+import { renderLayout } from './layout.js';
+import { generateDialog } from './dialog.js';
 import './style.css';
 
 renderLayout();
+
+const btn = document.querySelector('div#addItem');
+
+btn.addEventListener('mouseover', function() {
+    btn.style.backgroundColor = "rgb(100, 100, 100)";
+})
+
+btn.addEventListener('mouseout', function() {
+    btn.style.backgroundColor = "rgb(78, 78, 78)";
+})
+
+btn.addEventListener('mousedown', function() {
+    btn.style.backgroundColor = "rgb(120, 120, 120)";
+})
+
+btn.addEventListener('mouseup', function() {
+    btn.style.backgroundColor = "rgb(100, 100, 100)";
+})
+
+btn.addEventListener('click', () => { generateDialog(); });
