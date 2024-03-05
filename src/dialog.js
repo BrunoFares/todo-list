234a00169484ee9f082720c1574410473b4704ce
BrunoFares <faresbruno04@gmail.com>
2024-03-05 03:26:26 +0200

    Added the dialog box whenn the user wants to add a new list. For now, it appears on the dom but not in the viewpoint

--- src/dialog.js
+++ src/dialog.js
@@ -1,5 +1,29 @@
+const dialog = document.createElement('dialog');
+const form = document.createElement('form');
+form.action = 'GET';
+
+const label = document.createElement('label');
+label.htmlFor = 'listTitle';
+label.innerHTML = 'Name of the list'
+
+const input = document.createElement('input');
+input.type = 'text';
+input.id = 'listTitle';
+input.name = 'listTitle';
+
+const btn = document.createElement('button');
+btn.type = 'submit';
+btn.value = 'default';
+btn.id = 'confirmBtn';
+btn.innerHTML = 'Confirm';
+
+form.appendChild(label);
+form.appendChild(input);
+form.appendChild(btn);
+dialog.appendChild(form);
+
 function generateDialog() {
-    console.log("dialog shit");
+    document.body.appendChild(dialog);
 }
 
 export { generateDialog };
